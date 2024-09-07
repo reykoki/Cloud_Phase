28f89d8a4ec6e75b1b21e5e26b448bb1a9a679a9
reykoki <reykokimonster@gmail.com>
2024-09-06 21:07:57 -0600

    cloud_regime

---
+++ cloud_dataset.py
@@ -0,0 +1,163 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import sys
+import numpy as np
+import skimage
+from glob import glob
+import os
+import time
+import pytz
+from datetime import datetime, timedelta
+import wget
+import json
+sys.path.insert(1, './scripts')
+import pyproj
+from helper_functions import *
+from PIL import Image, ImageOps
+import warnings
+warnings.filterwarnings('ignore')
+from IPython.display import clear_output
+from satpy.writers import get_enhanced_image
+import s3fs
+from suntime import Sun
+from grab_goes import *
+from helper_functions import *
+from satpy import Scene
+from pyresample import create_area_def
+
+
+def check_sunrise_sunset(dt):
+    west_lon = -124.8
+    west_lat = 24.5
+    east_lon = -71.1
+    east_lat = 45.93
+    east = Sun(east_lat, east_lon)
+    west = Sun(west_lat, west_lon)
+    sunset = east.get_sunset_time(dt)
+    sunrise = west.get_sunrise_time(dt)
+    print('for the datetime {}:\nsunrise is at: {}\nsunset is at: {}'.format(dt, sunrise, sunset))
+    if sunrise > sunset:
+        sunset = west.get_sunset_time(dt + timedelta(days=1))
+    if sunrise > dt or sunset < dt:
+        raise ValueError('your request is before/after the sunrise/sunset for conus on {}'.format(dt) )
+    else:
+        sat_num = '16' # closer to sunset
+    return sunrise, sunset
+
+def get_proj():
+    lcc_proj = ccrs.LambertConformal(central_longitude=262.5,
+                                     central_latitude=38.5,
+                                     standard_parallels=(38.5, 38.5),
+                                     globe=ccrs.Globe(semimajor_axis=6371229,
+                                                      semiminor_axis=6371229))
+    return lcc_proj
+
+
+
+
+
+# get the Satpy Scene object
+def get_scn(fns, to_load, extent, res=3000, proj=get_proj(), reader='abi_l1b', print_info=False):
+    scn = Scene(reader=reader, filenames=fns)
+    scn.load(to_load, generate=False)
+    my_area = create_area_def(area_id='my_area',
+                              projection=proj,
+                              resolution=res,
+                              area_extent=extent
+                              )
+    if print_info:
+        print("Available channels in the Scene object:\n", scn.available_dataset_names())
+        print("\nAvailable composites:\n", scn.available_composite_names())
+        print("\nArea definitition:\n",my_area)
+    new_scn = scn.resample(my_area) # resamples datasets and resturns a new scene object
+    return new_scn
+
+def split_and_save(full_image, full_truth, fn_head, img_size=256):
+    yr = fn_head.split('s')[1][0:4]
+    n_row = int(full_image.shape[0]/img_size)
+    n_col = int(full_image.shape[1]/img_size)
+    full_image = full_image[0:int(n_row*img_size),0:int(n_col*img_size)][:]
+    full_truth = full_truth[0:int(n_row*img_size),0:int(n_col*img_size)][:]
+    fn_list = []
+    for row in range(n_row):
+        for col in range(n_col):
+            data = full_image[int(row*img_size):int((row+1)*img_size),int(col*img_size):int((col+1)*img_size)][:]
+            truth = full_truth[int(row*img_size):int((row+1)*img_size),int(col*img_size):int((col+1)*img_size)][:]
+            fn = '{}_{}_{}.tif'.format(fn_head, row, col)
+            density = "Light"
+            skimage.io.imsave('./cloud_data/data/{}/{}'.format(yr, fn), data)
+            skimage.io.imsave('./cloud_data/truth/{}/{}'.format(yr, fn), truth)
+            fn_list.append(fn)
+
+    return fn_list
+
+def get_one_hot(not_hot):
+    binary = np.take(np.eye(5), not_hot, axis=1)
+    binary = binary[1:,:,:]
+    binary = np.einsum('ijk->jki', binary)
+    return binary
+
+
+res = 2000 # 5km resolution
+extent = [-2.4e6, -1.6e6, 2.72e6, 1.472e6]
+cloud_mask = ["Phase"]
+composite = ["cimss_true_color_sunz_rayleigh"]
+
+
+def get_RGB(scn, composite):
+    RGB = get_enhanced_image(scn[composite]).data.compute().data
+    RGB = np.einsum('ijk->jki', RGB)
+    RGB[np.isnan(RGB)] = 0
+    return RGB
+
+def create_dataset(dt, sat_fns):
+    scn = get_scn(sat_fns[:-1], composite, extent, res) # get satpy scn object
+    conus_crs = scn[composite[0]].attrs['area'].to_cartopy_crs()
+    cloud_scn = get_scn([sat_fns[-1]], cloud_mask, extent, res, reader='abi_l2_nc')
+    RGB = get_RGB(scn, "cimss_true_color_sunz_rayleigh" )
+    cloud_phase = cloud_scn['Phase'].compute().data
+    cloud_phase[cloud_phase==255] = 0 # 255 seems like the number used for where radiance == nan
+    one_hot_mask = get_one_hot(cloud_phase) # from the scene object, extract RGB data for plotting
+    fn_head = sat_fns[0].split('C01_')[-1].split('.')[0].split('_c2')[0]
+    tif_fns = split_and_save(RGB, one_hot_mask, fn_head)
+
+
+def times_sunrise_to_sunset(dt):
+    west_lon = -124.8
+    west_lat = 24.5
+    east_lon = -71.1
+    east_lat = 45.93
+    east = Sun(east_lat, east_lon)
+    west = Sun(west_lat, west_lon)
+    sunset = east.get_sunset_time(dt)
+    sunrise = west.get_sunrise_time(dt)
+    if sunrise > sunset:
+        sunset = west.get_sunset_time(dt + timedelta(days=1))
+    times = []
+    new_time = sunrise
+
+    while new_time < sunset:
+        times.append(new_time)
+        new_time = times[-1] + timedelta(hours=1)
+    return times
+
+def main():
+    yr = '2023'
+    dns = np.arange(1,365)
+    for dn in dns:
+        dn_dt = pytz.utc.localize(datetime.strptime("{}{}".format(yr,dn), '%Y%j'))
+        times = times_sunrise_to_sunset(dn_dt)
+        print(times)
+        for dt in times:
+            print(dt)
+            sat_fns = download_goes(dt)
+            print(sat_fns)
+            create_dataset(dt, sat_fns)
+            for sat_fn in sat_fns:
+                os.remove(sat_fn)
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ scripts/grab_goes.py
@@ -0,0 +1,141 @@
+import numpy as np
+from glob import glob
+import os
+import pytz
+from datetime import datetime, timedelta
+from helper_functions import *
+import s3fs
+from suntime import Sun
+
+def get_first_closest_file(band, fns, dt, sat_num):
+    diff = timedelta(days=100)
+    matching_band_fns = [s for s in fns if band in s]
+    for fn in matching_band_fns:
+        s_e = fn.split('_')[3:5]
+        start = s_e[0]
+        s_dt = datetime.strptime(start[1:-3], '%Y%j%H%M')
+        s_dt = pytz.utc.localize(s_dt)
+        if diff > abs(s_dt - dt):
+            diff = abs(s_dt - dt)
+            best_start = start
+            best_end = s_e[1]
+            best_fn = fn
+    #fn_str = 'C{}_G{}_{}_{}'.format(band, sat_num, best_start, best_end)
+    fn_str = 'G{}_{}_{}'.format(sat_num, best_start, best_end[:-3])
+    return best_fn, fn_str
+
+def get_additional_band_file(band, fn_str, fns):
+    best_band_fn = [s for s in fns if band in s and fn_str in s]
+    return best_band_fn[0]
+
+def get_closest_file(fns, dt, sat_num, bands):
+    use_fns = []
+    band_init = 'C'+str(bands[0]).zfill(2)
+
+
+    best_band_fn, fn_str = get_first_closest_file(band_init, fns, dt, sat_num)
+    use_fns.append(best_band_fn)
+    for band in bands:
+        band = 'C'+str(band).zfill(2)
+        best_band_fn = get_additional_band_file(band, fn_str, fns)
+        use_fns.append(best_band_fn)
+    return use_fns
+
+# check if its between sunrise on the west coast and sunset on hte east coast
+# check that the sun is shining on entire CONUS
+def check_sunrise_sunset(dt):
+    west_lon = -124.8
+    west_lat = 24.5
+    east_lon = -71.1
+    east_lat = 45.93
+    east = Sun(east_lat, east_lon)
+    west = Sun(west_lat, west_lon)
+    sunset = east.get_sunset_time(dt)
+    sunrise = west.get_sunrise_time(dt)
+    print('for the datetime {}:\nsunrise is at: {}\nsunset is at: {}'.format(dt, sunrise, sunset))
+    if sunrise > sunset:
+        sunset = west.get_sunset_time(dt + timedelta(days=1))
+    if sunrise > dt or sunset < dt:
+        raise ValueError('your request is before/after the sunrise/sunset for conus on {}'.format(dt) )
+    else:
+        sat_num = '16' # closer to sunset
+    return sunrise, sunset
+
+def check_sunrise_sunset_lat_lon(dt, lat, lon):
+    sun = Sun(float(lat), float(lon))
+    sunset = sun.get_sunset_time(dt)
+    sunrise = sun.get_sunrise_time(dt)
+    if sunrise > sunset:
+        sunset = sun.get_sunset_time(dt + timedelta(days=1))
+    print('for the location ({}, {}):\nsunrise is at: {}\nsunset is at: {}'.format(lat, lon, sunrise, sunset))
+    if sunrise > dt:
+        raise ValueError('your request was before sunrise at ({}, {}) on {}, set sun_check=False to grab data out side of daylight'.format(lat, lon, dt) )
+    if sunset < dt:
+        raise ValueError('your request was after sunset at ({}, {}) on {}, set sun_check=False to grab data out side of daylight'.format(lat, lon, dt) )
+
+def get_filelist(dt, fs, lat, lon, sat_num, product, scope, bands):
+    hr, dn, yr = get_dt_str(dt)
+    full_filelist = fs.ls("noaa-goes{}/{}{}/{}/{}/{}/".format(sat_num, product, 'C', yr, dn, hr))
+    if sat_num == '17' and len(full_filelist) == 0:
+        if yr <= 2018:
+            sat_num = '16'
+            print("YOU WANTED 17 BUT ITS NOT LAUNCHED")
+        elif yr >= 2022:
+            sat_num = '18'
+        full_filelist = fs.ls("noaa-goes{}/ABI-L1b-Rad{}/{}/{}/{}/".format(sat_num, 'C', yr, dn, hr))
+    use_fns = get_closest_file(full_filelist, dt, sat_num, bands)
+    return use_fns
+
+def get_first_closest_file_mask(fns, dt, sat_num):
+    diff = timedelta(days=100)
+    for fn in fns:
+        s_e = fn.split('_')[3:5]
+        start = s_e[0]
+        s_dt = datetime.strptime(start[1:-3], '%Y%j%H%M')
+        s_dt = pytz.utc.localize(s_dt)
+        if diff > abs(s_dt - dt):
+            diff = abs(s_dt - dt)
+            best_start = start
+            best_end = s_e[1]
+            best_fn = fn
+    #fn_str = 'C{}_G{}_{}_{}'.format(band, sat_num, best_start, best_end)
+    fn_str = 'G{}_{}_{}'.format(sat_num, best_start, best_end[:-3])
+    return best_fn
+
+def get_filelist_mask(dt, fs, lat, lon, sat_num, product="ABI-L2-ACTP", scope="C"):
+    hr, dn, yr = get_dt_str(dt)
+    full_filelist = fs.ls("noaa-goes{}/{}{}/{}/{}/{}/".format(sat_num, product, 'C', yr, dn, hr))
+    use_fns = get_first_closest_file_mask(full_filelist, dt, sat_num)
+    return use_fns
+
+def download_goes(dt, lat=None, lon=None, sat_num='16', product='ABI-L1b-Rad', scope='C', check_sun=True, bands=list(range(1,4))):
+    # will check sunrise for specified lat/lon
+    if check_sun and lat and lon:
+        check_sunrise_sunset_lat_lon(dt, lat, lon)
+    # will check sunrise for CONUS
+    elif check_sun:
+        check_sunrise_sunset(dt)
+
+    goes_dir = './cloud_data/goes_temp/'
+    fs = s3fs.S3FileSystem(anon=True)
+
+    use_fns = get_filelist(dt, fs, lat, lon, sat_num, product, scope, bands)
+    use_fns_mask = get_filelist_mask(dt, fs, lat, lon, sat_num)
+    use_fns.append(use_fns_mask)
+    file_locs = []
+    print(use_fns_mask)
+    print(use_fns)
+    for file_path in use_fns:
+        fn = file_path.split('/')[-1]
+        dl_loc = goes_dir+fn
+        file_locs.append(dl_loc)
+        if os.path.exists(dl_loc):
+            print("{} already exists".format(fn))
+        else:
+            print('downloading {}'.format(fn))
+            fs.get(file_path, dl_loc)
+    if len(file_locs) > 0:
+        return file_locs
+    else:
+        print('ERROR NO FILES FOUND FOR TIME REQUESTED: ', dt)
+
